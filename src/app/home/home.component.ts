2b7e0f3edabf392f03ec8d7d10f0b23255b285c3
EngBasmaa <basmaelmasry263@gmail.com>
2024-10-20 16:47:15 +0300

    hi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit, OnDestroy {
+  private intervalId: any;
 
+  ngAfterViewInit() {
+    this.startSliderRotation();
+  }
+
+  ngOnDestroy() {
+    // Cleanup the interval when the component is destroyed
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startSliderRotation() {
+    this.intervalId = setInterval(() => {
+      this.rotateSlider();
+    }, 4000);
+  }
+
+  rotateSlider() {
+    const slider = document.querySelector('.slider') as HTMLElement;
+    const slides = slider.querySelectorAll('div');
+
+    if (slides.length > 0) {
+      const lastChild = slides[slides.length - 1].cloneNode(true) as HTMLElement;
+      slider.removeChild(slides[slides.length - 1]);
+
+      // Add last child to the beginning of the slider
+      slider.insertBefore(lastChild, slider.firstChild);
+
+      // Remove the 'firstSlide' class from all slides
+      slides.forEach((slide) => {
+        slide.classList.remove('firstSlide');
+      });
+
+      // Add 'firstSlide' class to the new first slide
+      lastChild.classList.add('firstSlide');
+    }
+  }
+  // .........................
+  submit() {
+    console.log('submit clicked');
+    alert("Your message has been sent successfully")
+  }
 }
